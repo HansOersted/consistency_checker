883313fe6fdb05af32372ec6c0b712e7562756ad
Zhe Shen <lucidshenzhe@gmail.com>
2024-03-26 16:33:53 +0900

    display the exact value of each parameter
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- consistency_checker.py
+++ consistency_checker.py
@@ -17,17 +17,44 @@ pid_params = read_yaml(pid_param_file_path)["/**"]["ros__parameters"]
 simulator_model_params = read_yaml(simulator_model_param_file_path)["/**"]["ros__parameters"]
 
 # compare the parameters
-results = {
+results = [
 #   "mpc_vehicle_model_type_consistency": mpc_params["vehicle_model_type"] == simulator_model_params["vehicle_model_type"],  # Should not compare directly. Modify later!!
-    "[MPC] steer_delay_difference": simulator_model_params["steer_time_delay"] - mpc_params["input_delay"],
-    "[MPC] steer_time_constant_difference": simulator_model_params["steer_time_constant"] - mpc_params["vehicle_model_steer_tau"],
-    "[MPC] acceleration_limit_difference": simulator_model_params["vel_rate_lim"] - mpc_params["acceleration_limit"],
-    "[MPC] max_steer_rate_lim_difference_by_curvature": simulator_model_params["steer_rate_lim"] - max(mpc_params["steer_rate_lim_dps_list_by_curvature"]) * (math.pi / 180),
-    "[MPC] max_steer_rate_lim_difference_by_velocity": simulator_model_params["steer_rate_lim"] - max(mpc_params["steer_rate_lim_dps_list_by_velocity"]) * (math.pi / 180),
-    "[PID] abs_max_acc_difference": simulator_model_params["vel_rate_lim"] - abs(pid_params["max_acc"]),
-    "[PID] abs_min_acc_difference": simulator_model_params["vel_rate_lim"] - abs(pid_params["min_acc"])
-}
+    ("[MPC] steer_delay_difference: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["steer_time_delay"] - mpc_params["input_delay"],
+        mpc_params["input_delay"],
+        simulator_model_params["steer_time_delay"])),
+
+    ("[MPC] steer_time_constant_difference: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["steer_time_constant"] - mpc_params["vehicle_model_steer_tau"],
+        mpc_params["vehicle_model_steer_tau"],
+        simulator_model_params["steer_time_constant"])),
+    
+    ("[MPC] acceleration_limit_difference: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["vel_rate_lim"] - mpc_params["acceleration_limit"],
+        mpc_params["acceleration_limit"],
+        simulator_model_params["vel_rate_lim"])),
+    
+    ("[MPC] max_steer_rate_lim_difference_by_curvature: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["steer_rate_lim"] - max(mpc_params["steer_rate_lim_dps_list_by_curvature"]) * (math.pi / 180),
+        max(mpc_params["steer_rate_lim_dps_list_by_curvature"]) * (math.pi / 180),
+        simulator_model_params["steer_rate_lim"])),
+
+    ("[MPC] max_steer_rate_lim_difference_by_velocity: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["steer_rate_lim"] - max(mpc_params["steer_rate_lim_dps_list_by_velocity"]) * (math.pi / 180),
+        max(mpc_params["steer_rate_lim_dps_list_by_velocity"]) * (math.pi / 180),
+        simulator_model_params["steer_rate_lim"])),    
+
+    ("[PID] max_acc_difference: {}, (controller: {}, simulator: {})".format(
+        simulator_model_params["vel_rate_lim"] - pid_params["max_acc"],
+        pid_params["max_acc"],
+        simulator_model_params["vel_rate_lim"])),
+
+    ("[PID] min_acc_difference: {}, (controller: {}, simulator: {})".format(
+        - simulator_model_params["vel_rate_lim"] - pid_params["min_acc"],
+        pid_params["min_acc"],
+        - simulator_model_params["vel_rate_lim"]))
+]
 
 # print the results
-for key, value in results.items():
-    print(f"{key}: {value}")
+for item in results:
+    print(item)
